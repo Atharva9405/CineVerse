93698952b6c3df72d898f078a43297dbdbe77b52
Atharva Nagore <atharvanagore2004@gmail.com>
2023-07-04 22:50:39 +0530

    added config files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
+import { ToastContainer } from "react-toastify";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  )
-}
+  const { themeMode } = useSelector((state) => state.themeMode);
 
-export default App
+  return (
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      >
+        <CssBaseline />
+      </ToastContainer>
+    </ThemeProvider>
+  );
+};
+
+export default App;
